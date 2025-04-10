f8c3436381b48f36ee1454d3beede03650d81bbb
Jason Lin <jclin2.2009@gmail.com>
2025-04-10 14:06:52 -0700

    reset styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import { Topbar } from "@/components/topbar";
-import { PredictionsTable } from "@/components/predictions-table";
-import PlayerPerformanceChart from "@/components/PlayerPerformanceChart";
-
 export default function Home() {
   return (
     <div className="p-4">
       <Topbar />
       <div className="mt-8">
         <h1 className="text-2xl font-bold mb-4">MLB Hits Predictions Leaderboard</h1>
-        <PredictionsTable />
+        table will go here
       </div>
      
       <div className="mt-8 border-t pt-8">
-        <PlayerPerformanceChart />
+        chart will go here
       </div>
     </div>
   );
