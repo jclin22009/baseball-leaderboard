22a424e7d65ea385bb2fd500f09669ef428225e0
Jason Lin <jclin2.2009@gmail.com>
2025-04-10 18:29:14 -0700

    add medals

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -120,7 +120,7 @@ export function SectionCards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       {topPredictions.map((prediction, index) => {
-        const rankLabels = ["1st Place", "2nd Place", "3rd Place", "Furthest Prediction"];
+        const rankLabels = ["1st Place 🥇", "2nd Place 🥈", "3rd Place 🥉", "Furthest Prediction"];
         
         // Extract first name
         const firstName = prediction.student.split(' ')[0];
